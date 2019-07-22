b9539c1d395cfbcc3fd1553daf7b598f19234434
the-skipper <zdraveskin@gmail.com>
2019-07-23 00:20:36 +0200

    api

--- server/index.js
+++ server/index.js
@@ -119,11 +119,6 @@ app.get("/payloads", async (req, res) => {
   res.send(await Payloads.find({}).exec());
 });
 
-app.post("/payloads", (req, res) => {
-  let payload = new Payloads(req.body);
-  payload.save();
-  res.status(201).send(payload);
-});
 // Handle all non API requests and delegate to React Frontend
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/../client/build/index.html"));
